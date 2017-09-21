60e4d0da367508add4f04f32a13e4f5ebca3036a
Andres Quintero <aquintero@oru.com.co>
2017-09-21 16:19:19 -0500

    Added config

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,4 +1,4 @@
 # !/bin/bash
 #PATH=/usr/local/bin
 #
-#curl http://serverstatus.mkitdigital.com/hookServer?server=1&disk=-1&mem=-1&cpu=-1&con=-1&ip=-1&memc=-1&token=pTX7s2h9FlmVB7lWDmAucUaN2A85NHO9JyZcvL2
+curl http://serverstatus.mkitdigital.com/hookServer?server=1&disk=40%|/dev/xvda1|&mem=1691/2000|84.55|&cpu=0.03&con=-1&ip=-1&memc=-1&token=pTX7s2h9FlmVB7lWDmAucUaN2A85NHO9JyZcvL2
