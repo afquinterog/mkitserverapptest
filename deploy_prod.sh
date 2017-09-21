e15c236fcfcda95373c0b0771933eb49c1e8d706
Andres Quintero <aquintero@oru.com.co>
2017-09-21 17:23:35 -0500

    Added config

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 #PATH=/usr/local/bin
 #
-curl http://serverstatus.mkitdigital.com/hookServer?server=1&disk=40%|/dev/xvda1|&mem=1691/2000|84.55|&cpu=0.03&con=-1&ip=-1&memc=-1&token=pTX7s2h9FlmVB7lWDmAucUaN2A85NHO9JyZcvL2
-wget http://serverstatus.mkitdigital.com/hookServer?server=1&disk=40%|/dev/xvda1|&mem=1691/2000|84.55|&cpu=0.03&con=-1&ip=-1&memc=-1&token=pTX7s2h9FlmVB7lWDmAucUaN2A85NHO9JyZcvL2
+sudo curl http://serverstatus.mkitdigital.com/hookServer?server=1&disk=40%|/dev/xvda1|&mem=1691/2000|84.55|&cpu=0.03&con=-1&ip=-1&memc=-1&token=pTX7s2h9FlmVB7lWDmAucUaN2A85NHO9JyZcvL2
+sudo wget http://serverstatus.mkitdigital.com/hookServer?server=1&disk=40%|/dev/xvda1|&mem=1691/2000|84.55|&cpu=0.03&con=-1&ip=-1&memc=-1&token=pTX7s2h9FlmVB7lWDmAucUaN2A85NHO9JyZcvL2
 echo 'deploy info'
